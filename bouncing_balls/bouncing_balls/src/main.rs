09da57ab84553689621389024cf733a989d29623
3d0a korkiv <mymail@mail.com>
2022-01-07 20:03:33 +0300

    previosus commit wasn right so here is some little fixes

--- bouncing_balls/bouncing_balls/src/main.rs
+++ bouncing_balls/bouncing_balls/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let result = bouncing_ball(f64::MAX, 0.6, 10.0);
+    let result = bouncing_ball(3.0, 0.66, 1.5);
     println!("{}", result);
 }
 
 fn bouncing_ball(h: f64,  bounce: f64,  window: f64) -> i32 {
     let mut bounce_counter    : i32 =   0;
     let mut direction         : i32 =  -1;
-    let mut decreased_height  : u64 =   h as u64;
+    let mut decreased_height        =   h ;
     loop {
         if h <= 0.0  {
             println!(" Float parameter 'h' in meters must be greater than 0");
@@ -21,11 +21,11 @@ fn bouncing_ball(h: f64,  bounce: f64,  window: f64) -> i32 {
             break;
         }
         else {
-            if direction < 0 && decreased_height > window as u64 {
+            if direction < 0 && decreased_height > window  {
                 bounce_counter = bounce_counter + 1;  
-                decreased_height = decreased_height * bounce as u64; 
+                decreased_height = decreased_height * bounce; 
             }
-            else if direction > 0 && decreased_height > window as u64 {
+            else if direction > 0 && decreased_height > window {
                 bounce_counter = bounce_counter + 1;
             }
             else {
