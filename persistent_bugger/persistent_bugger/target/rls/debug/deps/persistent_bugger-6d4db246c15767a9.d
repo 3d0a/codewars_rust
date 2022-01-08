e69d5fd6b93b4e924ee65951cf26e73cc5500c28
3d0a korkiv <mymail@mail.com>
2022-01-08 15:32:41 +0300

    created new project and added readme with task

---
+++ persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-6d4db246c15767a9.d
@@ -0,0 +1,5 @@
+/home/edoa/Documents/codewars/rust/persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-6d4db246c15767a9.rmeta: src/main.rs
+
+/home/edoa/Documents/codewars/rust/persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-6d4db246c15767a9.d: src/main.rs
+
+src/main.rs:
---
+++ persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-863696bdc4e8b050.d
@@ -0,0 +1,5 @@
+/home/edoa/Documents/codewars/rust/persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-863696bdc4e8b050.rmeta: src/main.rs
+
+/home/edoa/Documents/codewars/rust/persistent_bugger/persistent_bugger/target/rls/debug/deps/persistent_bugger-863696bdc4e8b050.d: src/main.rs
+
+src/main.rs:
