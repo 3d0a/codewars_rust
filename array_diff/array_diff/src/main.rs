cc344d808b61548e349b625f9b0bb17f53e2a7a4
3d0a korkiv <mymail@mail.com>
2022-01-11 23:18:30 +0300

    found out contains function in Vec object and used it

--- array_diff/array_diff/src/main.rs
+++ array_diff/array_diff/src/main.rs
@@ -2,37 +2,11 @@ fn main() {
     let discard_elements            = vec![3,4];
     let array                       = vec![1,2,3,1,2,10,133,7,4,4,4];
    // let array_with_no_1ns :Vec<i32> = array_diff(array, discard_elements);
-   println!("{:?}", array_dff(array, discard_elements));
+   println!("{:?}", array_diff(array, discard_elements));
 }
 fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
-    let mut result_vector :Vec<T> = vec![];
-    for element_a in a.iter(){
-        for element_b in b.iter() {
-            if element_a != element_b {
-
-            }
-        }
-    }
-    return result_vector;
-
-}
-
-fn array_dff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
-    let reuslt_vec = a.into_iter().filter(|x| show_dff(x, b.iter())).map(|x| x).collect();
+    let reuslt_vec = a.into_iter().filter(|x| ! b.contains(x)).map(|x| x).collect();
     return reuslt_vec;
 }
 
-fn show_dff<'a, I, T: PartialEq>(x: T, vector_iter: I) -> bool
-where
-    I: Iterator<Item = T>, {
-    for element in vector_iter {
-        if element != x {
-            continue;
-        }
-        else {
-            return false;
-        }
-    }
-    return true
-}
 
