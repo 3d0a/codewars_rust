81fd7638b98742c8a0de45ee26ce24518d59f4ac
3d0a korkiv <mymail@mail.com>
2022-01-12 18:01:52 +0300

    working version but code is too difficult need to make it less complicated

--- help_bookseller/help_booksaler/src/main.rs
+++ help_bookseller/help_booksaler/src/main.rs
@@ -2,16 +2,15 @@ use std::collections::HashMap;
 
 fn main() {
     let mut b = vec!["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
-    let mut c = vec!["A", "B", "C", "D"];
+    let mut c = vec!["A", "B", "C", "D", "E"];
     let hell = "hello world";
     println!("{}", stock_list(b,c));
     
 }
 
 fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
-    let mut stock_list_hashmap :HashMap<char, u32> = HashMap::new();
-    let mut stock_list_string                      = String::new(); 
-
+    let mut stock_list_hashmap :HashMap<char, u32> = HashMap::new(); 
+    let mut stock_list_string                      = String::new();
     for element in list_art {
         let element_as_vec         :Vec<&str> = element.split_whitespace().collect();
         let mut first_elem         :char      = element_as_vec[0].chars().next().unwrap();
@@ -19,24 +18,38 @@ fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
 
         match stock_list_hashmap.get(&first_elem) {
             Some(mut quontity) => {
-                let new_quontity :u32 = element_as_vec[1].parse().unwrap();
-                quontity = &(quontity + new_quontity);
+                let quontity = quontity + book_type_quintity;
+                stock_list_hashmap.remove(&first_elem);
+                stock_list_hashmap.insert(first_elem, quontity);
             }
             None => {
-                let new_quontity :u32 = element_as_vec[1].parse().unwrap();
-                stock_list_hashmap.insert(first_elem, new_quontity);
+                stock_list_hashmap.insert(first_elem, book_type_quintity);
             }
         }
     }
-    
-    for (key, value) in stock_list_hashmap {
-        stock_list_string.push('(');
-        stock_list_string.push(key);
-        stock_list_string.push('-');
-        let number_to_str :&str = &value.to_string();
-        stock_list_string.push_str(number_to_str);
-        stock_list_string.push(')');
-        stock_list_string.push(' ');
+    for character in list_cat{
+        let character :char = character.chars().next().unwrap();
+        match stock_list_hashmap.get(&character) {
+            Some(mut quontity) => {
+                stock_list_string = stock_list_string + &form_string(character, quontity);
+            }
+            None => {
+                stock_list_string = stock_list_string + &form_string(character, &0);
+            }
+        }
     }
+
+    return stock_list_string;
+}
+
+fn form_string(character :char, number :&u32) -> String {
+    let mut stock_list_string  = String::new();
+    stock_list_string.push('(');
+    stock_list_string.push(character);
+    stock_list_string.push('-');
+    let number_to_str :&str = &number.to_string();
+    stock_list_string.push_str(number_to_str);
+    stock_list_string.push(')');
+    stock_list_string.push(' ');
     return stock_list_string;
 }
