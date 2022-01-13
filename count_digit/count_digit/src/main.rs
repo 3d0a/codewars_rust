09d51e7817c0a2782c47778a5726b097f2b31bbb
3d0a korkiv <mymail@mail.com>
2022-01-13 22:36:55 +0300

    formated code a little bit

--- count_digit/count_digit/src/main.rs
+++ count_digit/count_digit/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
 
 fn nb_dig(n: i32, d: i32) -> i32 {
     // your code
-    let d_as_char   :char = d.to_string().parse::<char>().unwrap();
-    let squares :Vec<String>   = (0..n+1).map(|x| x*x).filter(|x| x.to_string().contains(d_as_char)).map(|x| x.to_string()).collect();
-    let quontity_of_digits :i32 = squares.join("").matches(d_as_char).count() as i32;
+    let d_as_char          :char          = d.to_string().parse::<char>().unwrap();
+    let squares            :Vec<String>   = (0..n+1).map(|x| x*x).filter(|x| x.to_string().contains(d_as_char)).map(|x| x.to_string()).collect();
+    let quontity_of_digits :i32           = squares.join("").matches(d_as_char).count() as i32;
     return quontity_of_digits;
 }
