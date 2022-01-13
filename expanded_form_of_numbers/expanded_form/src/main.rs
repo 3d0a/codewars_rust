eb8beeee1e2675ffdf6e118fd3b9014e452b60fb
3d0a korkiv <mymail@mail.com>
2022-01-14 02:34:11 +0300

    small refactoring

--- expanded_form_of_numbers/expanded_form/src/main.rs
+++ expanded_form_of_numbers/expanded_form/src/main.rs
@@ -7,10 +7,10 @@ fn expanded_form(n: u64) -> String {
     let mut starting_pow     :u32 = (vector_of_digits.len() - 1) as u32;
     let base                 :u64 = 10;
     let string_vec           :Vec<String> = vector_of_digits.into_iter()
-                                                       .map(|mut x| {x = x*base.pow(starting_pow); if starting_pow !=0 {starting_pow-=1}; x})
-                                                       .filter(|x| *x != 0)
-                                                       .map(|x| x.to_string())
-                                                       .collect();
+                                                            .map(|mut x| {x = x*base.pow(starting_pow); if starting_pow !=0 {starting_pow-=1}; x})
+                                                            .filter(|x| *x != 0)
+                                                            .map(|x|     x.to_string())
+                                                            .collect();
     let result_string = string_vec.join(" + ");
     return result_string;
 } 
