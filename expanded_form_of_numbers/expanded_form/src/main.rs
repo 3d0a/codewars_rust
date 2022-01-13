19d34050b11853f26a45b5459845e1caf32ad291
3d0a korkiv <mymail@mail.com>
2022-01-14 02:20:08 +0300

    small fix that terminates '0' from string

--- expanded_form_of_numbers/expanded_form/src/main.rs
+++ expanded_form_of_numbers/expanded_form/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("{}", expanded_form(7442));
+    println!("{}", expanded_form(74402));
 }
 
 fn expanded_form(n: u64) -> String {
@@ -7,7 +7,9 @@ fn expanded_form(n: u64) -> String {
     let mut starting_pow     :i32 = (vector_of_digits.len() - 1) as i32;
     let base                 :i32 = 10;
     let string_vec           :Vec<String> = vector_of_digits.into_iter()
-                                                       .map(|mut x| {x = x*base.pow(starting_pow as u32);starting_pow = starting_pow -1; x.to_string()})
+                                                       .map(|mut x| {x = x*base.pow(starting_pow as u32);starting_pow = starting_pow -1; x})
+                                                       .filter(|x| *x != 0)
+                                                       .map(|x| x.to_string())
                                                        .collect();
     let result_string = string_vec.join(" + ");
     return result_string;
