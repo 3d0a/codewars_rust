3c72bb3be64dc77c2f8367d8c74313b691a2ac9f
3d0a korkiv <mymail@mail.com>
2022-01-16 17:06:13 +0300

    changed current_count type to u64

--- pile_of_cubes/pile_of_cubes/src/main.rs
+++ pile_of_cubes/pile_of_cubes/src/main.rs
@@ -1,22 +1,21 @@
 fn main() {
-    println!("{}", u64::MAX);
     println!("{}",find_nb(91716553919377));
 }
 
 fn find_nb(m: u64) -> i32 {
     let mut volume         :u64 = 0;
-    let mut current_count  :i32 = 1;
+    let mut current_count  :u64 = 1;
     loop {
-        match current_count.checked_mul(current_count.pow(3)) {
+        match current_count.checked_mul(current_count.pow(2)) {
             Some(_v) => {
                 volume = volume + current_count.pow(3) as u64;
             }
             None => {
-                break;
+                return current_count as i32;
             }
         }
         if volume == m {
-            return current_count;
+            return current_count as i32;
         }
         else if volume > m {
             break;
