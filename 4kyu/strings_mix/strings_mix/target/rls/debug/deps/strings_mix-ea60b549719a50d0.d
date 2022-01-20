0f9452252387279d5a0205154f8445ed518bcf9a
Ivan Korkunov <korkunov@mail.ru>
2022-01-20 17:12:27 +0300

    commit of rust infrastructure

--- 4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.d
+++ 4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.d
@@ -1,5 +1,5 @@
-/home/edoa/Documents/codewars/rust/4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.rmeta: src/main.rs
+/home/ikorkunov/rust_codewars/4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.rmeta: src/main.rs
 
-/home/edoa/Documents/codewars/rust/4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.d: src/main.rs
+/home/ikorkunov/rust_codewars/4kyu/strings_mix/strings_mix/target/rls/debug/deps/strings_mix-ea60b549719a50d0.d: src/main.rs
 
 src/main.rs:
