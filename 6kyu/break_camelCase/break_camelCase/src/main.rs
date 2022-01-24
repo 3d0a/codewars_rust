95b0a109fa2dc9381112db6da5a09b8b35d419bc
edoa <korkunov@mail.ru>
2022-01-24 09:38:43 +0300

    decreased size of code

--- 6kyu/break_camelCase/break_camelCase/src/main.rs
+++ 6kyu/break_camelCase/break_camelCase/src/main.rs
@@ -4,18 +4,11 @@ fn main() {
 
 fn solution(s: &str) -> String {
     let mut final_string :String = String::new();
-    let alphabet                 = (b'A'..=b'Z').map(|c| c as char)
-                                                .filter(|c| c.is_alphabetic())
-                                                .collect::<Vec<_>>();
-
     for character in s.chars() {
-        if alphabet.contains(&character) {
+        if character.is_uppercase() {
             final_string.push(' ');
-            final_string.push(character);
         }
-        else {
             final_string.push(character);
-        }
     }
     return final_string.trim().to_owned();
 }
