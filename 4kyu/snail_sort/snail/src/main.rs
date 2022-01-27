858d9e6da325b5418408b334c35a30f9277036b4
edoa <korkunov@mail.ru>
2022-01-27 13:29:02 +0300

    make it work prop. change of logick in go_right function

--- 4kyu/snail_sort/snail/src/main.rs
+++ 4kyu/snail_sort/snail/src/main.rs
@@ -1,34 +1,48 @@
 fn main() {
     println!("Hello, world!");
     let square = &[
-        vec![1,2,3],
-        vec![4,5,6],
-        vec![7,8,9],
+        vec![1, 2, 3, 4, 5],
+        vec![6, 7, 8, 9, 10],
+        vec![11, 12, 13, 14, 15],
+        vec![16, 17, 18, 19, 20],
+        vec![21, 22, 23, 24, 25],
     ];
     snail(square);
 }
 
 fn snail(matrix: &[Vec<i32>]) -> Vec<i32> {
-    let matrix_size = matrix.len() - 1;
+    if matrix.len() == 1 {
+        if matrix[0].len() == 0 {
+            return vec![];
+        }
+        else {
+            return vec![matrix[0][0]];
+        }
+    }
+    let matrix_size = matrix.len();
     let mut one_demention_vec  = vec![];
-    let mut right :usize = matrix_size;
-    let mut down  :usize = matrix_size;
-    let mut left  :usize = matrix_size;
-    let mut up    :usize = matrix_size;
-    let up_coordinate = &(0, 0);
+    let mut right :usize   = matrix_size - 1;
+    let mut down  :usize   = matrix_size;
+    let mut left  :usize   = matrix_size;
+    let mut up    :usize   = matrix_size;
+    let mut up_coordinate  = (0, 0);
     loop {
-        let right_coordinate = &go_right(matrix, &mut one_demention_vec, up_coordinate, right);
+        let right_coordinate = &go_right(matrix, &mut one_demention_vec, &up_coordinate, right);
+        if up == 0 || down == 0    {break}
         up    -= 1;
-        if up == 0 {break}
-        let down_coordinate  = &go_down(matrix, &mut one_demention_vec, right_coordinate, down);
-        right -= 1;
-        if right == 0 {break}
-        let left_coordinate  = &go_left(matrix, &mut one_demention_vec, down_coordinate, left);
         down  -= 1;
-        if down == 0 {break}
-        let up_coordinate    = &go_up(matrix, &mut one_demention_vec, left_coordinate, up);
+        let down_coordinate = &go_down(matrix, &mut one_demention_vec, right_coordinate, down);
+        if right == 0 || left == 0 {break}
+        right -= 1;
         left  -= 1;
-        if left == 0 {break}
+        let left_coordinate  = &go_left(matrix, &mut one_demention_vec, down_coordinate, left);
+        if down == 0 || up == 0    {break}
+        down  -= 1;
+        up    -= 1;
+        up_coordinate = go_up(matrix, &mut one_demention_vec, left_coordinate, up);
+        if left == 0 || right == 0 {break}
+        left  -= 1;
+        right -= 1;
     }
     println!("{:?}", one_demention_vec);
     return one_demention_vec;
@@ -37,10 +51,12 @@ fn snail(matrix: &[Vec<i32>]) -> Vec<i32> {
 fn go_right(matrix: &[Vec<i32>], one_demention_vec: &mut Vec<i32>, coordinate: &(usize, usize), steps: usize) -> (usize, usize) {
     // coordinate.0 - is LINE
     // coordinate.1 - is number of Vec
-    for i in coordinate.0..=coordinate.0+steps {
+    let mut start = coordinate.0;
+    let mut end   = coordinate.0+steps;
+    for i in start..= end {
         one_demention_vec.push(matrix[coordinate.1][i]);
     }
-    return (coordinate.0+steps, coordinate.1+1 );
+    return (end, coordinate.1+1 );
 } 
 
 fn go_down(matrix: &[Vec<i32>], one_demention_vec: &mut Vec<i32>, coordinate: &(usize, usize), steps: usize) -> (usize, usize) {
@@ -67,5 +83,5 @@ fn go_up(matrix: &[Vec<i32>], one_demention_vec: &mut Vec<i32>, coordinate: &(us
     for i in (coordinate.1-steps..=coordinate.1 - 1).rev() {
         one_demention_vec.push(matrix[i][coordinate.0]);
     }
-    return (coordinate.0, coordinate.1-steps);
+    return (coordinate.0 + 1, coordinate.1-steps);
 } 
